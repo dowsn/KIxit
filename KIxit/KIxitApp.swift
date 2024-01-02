bef89ef08d1eab64e0a95fe7dc18e5dab129a7af
Lukas <lukas@treat.agency>
2024-01-02 22:04:06 +0100

    Initial Commit

---
+++ KIxit/KIxitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KIxitApp.swift
+//  KIxit
+//
+//  Created by Lukas Meinhart on 02.01.24.
+//
+
+import SwiftUI
+
+@main
+struct KIxitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
