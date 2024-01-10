a8fd130e29561c80dde6f8169415b7d682157a33
Lukas <lukas@treat.agency>
2024-01-10 09:18:15 +0100

    commit

--- KIxit/Views/ExhibitionsView.swift
+++ KIxit/Views/ExhibitionsView.swift
@@ -11,7 +11,7 @@ struct ExhibitionsView: View {
     @ObservedObject var openai = OpenAIController()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, d World!")
     }
 }
 
